532a3e36403e246681386e87a1fe5f2d0e0ccf5a
Savva <nightowlsavva@gmail.com>
2017-10-28 22:17:54 +0300

    nothing

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-unction hideInfo(){
-  document.getElementById('eurasia').style.display = "none"
-  document.getElementById('africa').style.display = "none"
-  document.getElementById('northAmerica').style.display = "none"
-  document.getElementById('southAmerica').style.display = "none"
-  document.getElementById('australia').style.display = "none"
-=======
 function hideInfo(){
   document.getElementById('news1').style.display = "none"
   document.getElementById('news2').style.display = "none"
   document.getElementById('news3').style.display = "none"
   document.getElementById('news4').style.display = "none"
   document.getElementById('news5').style.display = "none"
->>>>>>> 7c05e63747b94dc381fc6ef39e9ac6cb645dd953
-
 }
 
 function revealInfo(element){
@@ -22,23 +12,16 @@ function revealInfo(element){
                 document.getElementById('news2'),
                 document.getElementById('news3'),
                 document.getElementById('news4'),
-                document.getElementById('news5'),
-                document.getElementById('choosing')];
+                document.getElementById('news5')];
 
 if (info.style.display == "none"){
   for (var i = 0; i < allElements.length; i++) {
     allElements[i].style.display = "none"
   }
   info.style.display = "block";
-};
 }
-
-
-
-
-
 
 var el = document.getElementById('subscribe')
 el.addEventListener('click', function() {
- prompt("what is your email");
+ prompt("what is your email:", "");
  };
