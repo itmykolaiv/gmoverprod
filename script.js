7c05e63747b94dc381fc6ef39e9ac6cb645dd953
Savva <nightowlsavva@gmail.com>
2017-10-28 18:32:34 +0300

    Savva improved news

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function hideInfo(){
-  document.getElementById('eurasia').style.display = "none"
-  document.getElementById('africa').style.display = "none"
-  document.getElementById('northAmerica').style.display = "none"
-  document.getElementById('southAmerica').style.display = "none"
-  document.getElementById('australia').style.display = "none"
+  document.getElementById('news1').style.display = "none"
+  document.getElementById('news2').style.display = "none"
+  document.getElementById('news3').style.display = "none"
+  document.getElementById('news4').style.display = "none"
+  document.getElementById('news5').style.display = "none"
 
 }
 
 function revealInfo(element){
   info = document.getElementById(element);
-  allElements = [document.getElementById('eurasia'),
-                document.getElementById('africa'),
-                document.getElementById('northAmerica'),
-                document.getElementById('southAmerica'),
-                document.getElementById('australia'),
+  allElements = [document.getElementById('news1'),
+                document.getElementById('news2'),
+                document.getElementById('news3'),
+                document.getElementById('news4'),
+                document.getElementById('news5'),
                 document.getElementById('choosing')];
 
 if (info.style.display == "none"){
