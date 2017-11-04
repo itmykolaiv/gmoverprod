9d99565e51c73f16b8bf37dc800c060ae58cdac5
AllaSmyshlyaeva <alla.smyshlyaeva@gmail.com>
2017-11-04 18:24:24 +0200

    add req to api

--- script.js
+++ script.js
@@ -8,24 +8,23 @@ var config = {
 }
 
 //Send request
-function send_request(command, query) {
-  fetch(config.baseUrl + '/'+command+'/?api_key='+config.app_key+'&format=json'+query)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-    })
-    .catch( function() {
-      console.log("Somthing wrong!!!");
-    });
+function send_request(command, query, cb) {
+  var baseUrl = 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/gmoverprod';
+  
+  fetch(baseUrl + '/' + command + '/?api_key=db13960f716ed8847edfaa64dad63ac7d63f7e1a&format=json&' + query)
+      .then(function(response) {
+          return response.json();
+      })
+      .then(cb)
+      .catch( function() {
+          
+      });
 }
 
 function build_items () {
   var cont = document.querySelector('.js_items-container');
-  //Fill our container
+//Fill our container
 }
-
 function creator_item () {
   var div = document.createElement('div');
   //add another attributes to div var.
