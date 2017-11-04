40146f95eb26a6c5db4f6d1ed841dfa7b8e915e3
ViolaPastirya <vpastirya@gmail.com>
2017-11-04 18:10:37 +0200

    added button with adding news

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function showSubscribeAlert() {
   prompt("What is your email: ");
 }
 
+function showNewsAlert() {
+	var name_news = prompt("What news do you want to offer: ");
+	if (name_news == "") {
+		return showNewsAlert();
+	}
+	var content = prompt("What is your news about: ");
+	if (content == "") {
+		return showNewsAlert();
+	}
+	var time = prompt("When did it happen: ");
+	if (time == "") {
+		return showNewsAlert();
+	}
+	alert(name_news + "\n" + content + "\n" + time);
+}
+
+
 var config = {
   baseUrl: 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/gmoverprod',
   app_key: 'db13960f716ed8847edfaa64dad63ac7d63f7e1a'
