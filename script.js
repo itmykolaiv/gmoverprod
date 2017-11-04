80e50cacccc55115d309cb219943c423995aadd4
Denis Bilchenko <dbilchenko@geeksforless.net>
2017-11-04 22:52:01 +0200

    add api req

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ var config = {
 //Send request
 function send_request(command, query, cb) {
   var baseUrl = 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/gmoverprod';
-  
-  fetch(baseUrl + '/' + command + '/?api_key=db13960f716ed8847edfaa64dad63ac7d63f7e1a&format=json&' + query)
+  //https://www.giantbomb.com/api/games/?api_key=db13960f716ed8847edfaa64dad63ac7d63f7e1a&format=json&limit=10&sort=original_release_date:desc
+
+  fetch(baseUrl + '/' + command + '/?api_key=db13960f716ed8847edfaa64dad63ac7d63f7e1a&format=json' + query)
       .then(function(response) {
           return response.json();
       })
